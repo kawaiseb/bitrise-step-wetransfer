cb9f9f7d9eb06d87a5273ca46c31f6e12aadd97a
kawaiseb <spousse@pagesjaunes.fr>
2018-02-08 18:21:09 +0100

    wip

--- step.sh
+++ step.sh
@@ -8,5 +8,5 @@ ls $THIS_SCRIPT_DIR
 
 npm install
 npm install wetransfert --save
-$THIS_SCRIPT_DIR/upload.js "${WTU_DEBUG}" "${WTU_MAILSENDER}" "${WTU_MAILRECEIVER}" "${WTU_FILEPATH}" "${WTU_MESSAGE}" "${WTU_LANGUAGE}")
+$THIS_SCRIPT_DIR/upload.js "${WTU_DEBUG}" "${WTU_MAILSENDER}" "${WTU_MAILRECEIVER}" "${WTU_FILEPATH}" "${WTU_MESSAGE}" "${WTU_LANGUAGE}"
 
