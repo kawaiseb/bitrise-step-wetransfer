29f5f867ebd4622efb1682904fb656f313263e39
kawaiseb <spousse@pagesjaunes.fr>
2018-02-22 16:21:24 +0100

    fix(step.h):adding prefix to install the package in the right dir

--- step.sh
+++ step.sh
@@ -3,6 +3,6 @@ set -ex
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-npm install wetransfert --save
+npm install --prefix $THIS_SCRIPT_DIR wetransfert --save
 
 $THIS_SCRIPT_DIR/upload.js "${wtu_debug_mode}" "${wtu_mailsender}" "${wtu_mailreceiver}" "${wtu_filepath}" "${wtu_message}" "${wtu_language}"
